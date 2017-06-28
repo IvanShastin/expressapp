9133c10d7b1217e25cb6dcc5715ee8a07cc6b5ef
Ivan Shastin <ivan.shastin@gmail.com>
2017-06-28 22:57:08 +0200

    add quote of the date to about view and add static files middleware

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
-const handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
+const handlebars = require("express-handlebars").create({
+  defaultLayout: "main"
+});
 
 const app = express();
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-app.get('/', (request, response) => {
-    response.render('home');
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (request, response) => {
+  response.render("home");
 });
 
-app.get('/about', (request, response) => {
-    response.render('about');
+var fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple."
+];
+
+app.get("/about", (request, response) => {
+  var randomFortune = 
+    fortunes[Math.floor(Math.random() * fortunes.length)];
+  response.render("about", { fortune: randomFortune });
 });
 
 app.use((request, response) => {
-    response.status(404);
-    response.render('404');
+  response.status(404);
+  response.render("404");
 });
 
 app.use((error, request, response, next) => {
-    if(error)console.error(error); 
-    response.status(500);
-    response.render('500');
+  if (error) console.error(error);
+  response.status(500);
+  response.render("500");
 });
 
-app.listen(app.get('port'), () => {
-    console.log('Express started on http://localhost:' + app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("Express started on http://localhost:" + app.get("port"));
 });
